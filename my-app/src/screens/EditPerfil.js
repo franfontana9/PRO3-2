0db7ee5c3847ba7aa4665c890d7d5ba45fb1ee29
santiagoscilingo <santiago.scilingo>
2023-06-15 10:52:46 -0300

    avances

--- my-app/src/screens/EditPerfil.js
+++ my-app/src/screens/EditPerfil.js
@@ -65,8 +65,8 @@ class EditPerfil extends Component {
         console.log(this.state.usuario);
         return(
             <View style={styles.contenedor}>
-                   <Text style={styles.titulo} > Editar Perfil </Text>
-            <Text>Si no introducis una nueva contraseña, se mantedra la anterior CAMBIAR</Text>
+                   <Text style={styles.titulo} > Edit Profile </Text>
+                   <Text style={styles.texto5}>If you don't enter a new password, the previous one will remain UNCHANGED.</Text>
                   <TextInput style={styles.texto2}
              placeholder='password' 
              keyboardType='password'
@@ -88,11 +88,11 @@ class EditPerfil extends Component {
                
         
               <  TouchableOpacity onPress={()=>this.actualizar(this.state.inputPassword,this.state.inputUsername,this.state.inputBio)}> 
-              <Text style={styles.texto7}> Editar Perfil </Text>
+              <Text style={styles.texto7}> Edit Profile </Text>
             </TouchableOpacity>
-            {this.state.errorAlEliminar == false ? <Text> </Text> :  <Text> Esta es una operación sensible, volvé a iniciar sesión para eliminar tu perfil</Text>}
+            {this.state.errorAlEliminar == false ? <Text> </Text> :  <Text style={styles.texto6}>Deleting your profile requires a delicate operation. Log in again to proceed.</Text>}
            
-<Text style={styles.texto8} onPress={() => this.props.navigation.navigate("Profile")}> Volve al perfil</Text>
+<Text style={styles.texto8} onPress={() => this.props.navigation.navigate("Profile")}> Return to Profile</Text>
          </View>
        
         )
@@ -101,5 +101,72 @@ class EditPerfil extends Component {
 
 
 const styles = StyleSheet.create({
-})
+    contenedor: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+    },
+    titulo: {
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginBottom: 20,
+    },
+    texto2: {
+      width: '80%',
+      height: 40,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 5,
+      paddingLeft: 10,
+      marginBottom: 10,
+    },
+    texto3: {
+      width: '80%',
+      height: 40,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 5,
+      paddingLeft: 10,
+      marginBottom: 10,
+    },
+    texto4: {
+      width: '80%',
+      height: 100,
+      borderWidth: 1,
+      borderColor: '#ccc',
+      borderRadius: 5,
+      paddingLeft: 10,
+      marginBottom: 10,
+      textAlignVertical: 'top',
+    },
+    texto5: {
+        fontSize: 14,
+        marginBottom: 10,
+        marginLeft: 10, 
+      },
+      texto6: {
+        fontSize: 14,
+        marginTop: 10,
+        marginLeft: 10, 
+      },
+      
+    texto7: {
+      backgroundColor: '#007AFF',
+      color: '#fff',
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      borderRadius: 5,
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    texto8: {
+      marginTop: 20,
+      color: '#007AFF',
+      fontSize: 16,
+      fontWeight: 'bold',
+      textDecorationLine: 'underline',
+    },
+  });
+  
 export default EditPerfil
