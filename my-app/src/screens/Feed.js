0587ffafab2f3d591cd900af79ecb9b29bf14a08
santiagoscilingo <santiago.scilingo>
2023-06-15 06:31:51 -0300

    Estilo feed

--- my-app/src/screens/Feed.js
+++ my-app/src/screens/Feed.js
@@ -4,54 +4,60 @@ import { auth, db } from '../firebase/config'
 import Posteos from '../components/Posteos'
 
 class Feed extends Component {
-
-    constructor(props){
-        super()
-        this.state={
-            posts:[]
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      posts: []
     }
+  }
 
-
-    componentDidMount(){
-        db.collection('posts')
-        .orderBy('createdAt','desc')
-        // .where('owner','==',auth.currentUser.email)
-        // .limit(2)
-        .onSnapshot(docs=>{
-
-            let arrayDocs=[]
-
-            docs.forEach(docs=>arrayDocs.push({
-                id: docs.id,
-                data: docs.data()
-            }))
-            console.log(arrayDocs);
-            this.setState({
-                posts:arrayDocs
-            })
+  componentDidMount() {
+    db.collection('posts')
+      .orderBy('createdAt', 'desc')
+      .onSnapshot(docs => {
+        let arrayDocs = []
+        docs.forEach(docs =>
+          arrayDocs.push({
+            id: docs.id,
+            data: docs.data()
+          })
+        )
+        console.log(arrayDocs)
+        this.setState({
+          posts: arrayDocs
         })
-    }
+      })
+  }
+
   render() {
     return (
-        <View style={styles.container1}>
-        <Text>Bienvenidos a IG</Text>
-        <Text>Mira todos los posteos!</Text>
-        <Posteos
-                data={this.state.posts}
-                navigation={this.props.navigation}
-            />
-
+      <View style={styles.container}>
+        <View style={styles.centered}>
+          <Text style={styles.welcomeText}>Bienvenidos a IG</Text>
+          <Text style={styles.postText}>Mira todos los posteos!</Text>
         </View>
-        )
+        <Posteos data={this.state.posts} navigation={this.props.navigation} />
+      </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
-    container1:{
-        flex:1,   
-    }
-
+  container: {
+    flex: 1
+  },
+  centered: {
+    alignItems: 'center',
+    marginTop: 40
+  },
+  welcomeText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  postText: {
+    fontSize: 18
+  }
 })
 
 export default Feed
